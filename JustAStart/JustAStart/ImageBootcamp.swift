5ad3ee90d14f0becb837a36ca27b2e96504cacea
Arsham1024 <Arsham512@gmail.com>
2023-07-22 17:27:33 -0700

    images added

---
+++ JustAStart/JustAStart/ImageBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  ImageBootcamp.swift
+//  JustAStart
+//
+//  Created by Arsham Mehrani on 7/22/23.
+//
+
+import SwiftUI
+
+struct ImageBootcamp: View {
+    var body: some View {
+        Image("Margot")
+            .frame(width: 20, height: 20)
+    }
+}
+
+struct ImageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageBootcamp()
+    }
+}
